3d96d91c3ade16d1c1425381d1c9b44631efb9cb
Fabricio MSS <47029274+fabriciodf@users.noreply.github.com>
2025-04-23 14:23:51 -0300

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,1 +1,13 @@
+from fastapi import FastAPI
+import logging
 
+app = FastAPI()
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+@app.get("/ping")
+def ping():
+    return {"message": "pong"}
+@app.get("/")
+def home():
+    return {"message": "Hello World"}
