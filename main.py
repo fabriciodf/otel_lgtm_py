183c9c74cfda06443b51b60880d4c552e3721225
Fabricio MSS <47029274+fabriciodf@users.noreply.github.com>
2025-04-23 14:22:13 -0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,12 @@
+from fastapi import FastAPI
+import logging
+app = FastAPI()
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+@app.get("/ping")
+def ping():
+    return {"message": "pong"}
+@app.get("/")
+def home():
+    return {"message": "Hello World"}
